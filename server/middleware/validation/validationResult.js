7e3423adc6d008ed5191aca300349f7ca4d5a7ad
victor <viheanaco@gmail.com>
2019-02-01 14:15:48 +0100

    setup heroku

--- server/middleware/validation/validationResult.js
+++ server/middleware/validation/validationResult.js
@@ -2,15 +2,17 @@ import { validationResult } from 'express-validator/check';
 
 // A function that allows us to validate all the results returned by validate.js
 function validateResult(req, res, next) {
+  const errors = validationResult(req);
 
   // if we have any errors
   if (!errors.isEmpty()) {
-    return res.status(400).json({
-      status: 400,
-
-      // return an array of the errors and our custom message
-      error: errors.array()[0],
-    });
+    return res
+      .status(400)
+      .json({
+        status: 400,
+        // return an array of the errors and our custom message
+        error: errors.array()[0],
+      });
   }
 
   // pass control to the next middleware
--- server/routes/office/index.js
+++ server/routes/office/index.js
@@ -11,18 +11,22 @@ const router = express.Router();
 const { checkOfficeId, checkAddOffice, checkEditOffice } = officeValidation;
 
 //  1. POST - create a political office
-router.post('/', checkAddOffice, validationResult, officeController.createOffice);
+router.post('/', checkAddOffice, officeController.createOffice);
 
 //  2. GET - get all political ofices
+// router.get('/', checkAddOffice, officeController.getAllOffices);
 router.get('/', officeController.getAllOffices);
 
 //  3. GET - get a political ofices
+// router.get('/:id', officeController.getSingleOffice);
 router.get('/:id', checkOfficeId, validationResult, officeController.getSingleOffice);
 
 //  4. PATCH- update a political office
+// router.patch('/:id/:name', checkEditOffice, officeController.updateOffice);
 router.patch('/:id/:name', checkOfficeId, checkEditOffice, validationResult, officeController.updateOffice);
 
 //  5. DELETE- delete a political office
-router.delete('/:id', checkOfficeId, validationResult, officeController.deleteOffice);
+router.delete('/:id', officeController.deleteOffice);
+// router.delete('/:id', checkOfficeId, validationResult, officeController.deleteOffice);
 
 export default router;
