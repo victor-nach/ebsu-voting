dd8ae534cbab2c7b59789173d8a4971db87de679
victor <viheanaco@gmail.com>
2019-02-01 07:44:19 +0100

    ft: users would see a modal that would allow them to vot for candidates

---
+++ UI/assets/js/dashboard_vote.js
@@ -0,0 +1,22 @@
+// pop up notification
+
+let logInModal = document.querySelector('.modal-box.login');
+let logInLink = document.querySelector('.log-in-link');
+let closeBtn = document.querySelector('.close-button');
+
+
+// function to add show class to the login modal
+let showlogInModal = () => logInModal.classList.add('show');
+
+// function to add show class to the sign up modal
+let showsignUpModal = () => signUpModal.classList.add('show');
+
+// function to remove modal from login and sign up modal
+let closeModal = () => {
+    logInModal.classList.remove('show');
+};
+
+// attach event listener to the log in links and add show class on click
+logInLink.addEventListener('click', showlogInModal);
+
+closeBtn.addEventListener("click", closeModal);
