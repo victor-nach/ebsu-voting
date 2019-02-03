ea840f8d0ad7393bc95afb63cf1a4c3132882ece
victor <viheanaco@gmail.com>
2019-02-03 16:41:38 +0100

    changes made to test script

--- server/test/office.spec.js
+++ server/test/office.spec.js
@@ -1,15 +1,21 @@
-import { expect } from 'chai';
-import supertest from 'supertest';
+import chai from 'chai';
+// import supertest from 'supertest';
+import chaiHttp from 'chai-http';
 import server from '../app';
 
-const api = supertest(server);
+const { expect } = chai;
+chai.use(chaiHttp);
+
+
+// const api = supertest(server);
+const api = chai.request(server);
 
 describe('Offices', () => {
   describe('GET / Office', () => {
     it('it should create a political party', (done) => {
       api.get('/api/v1/office')
-        .expect(200)
         .end((err, res) => {
+          if (err) return done(err);
           expect(res.status).to.equal(200);
           done();
         });
